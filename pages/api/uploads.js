86643e19f477a7cb1446aaa2d9a668089c0df9cc
Martín M <skydiver@users.noreply.github.com>
2021-11-30 11:57:19 -0300

    start from boilerplate

---
+++ pages/api/uploads.js
@@ -0,0 +1,53 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
+
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  }
+});
+
+apiRoute.use(upload.single('attachment'));
+
+apiRoute.post(async (req, res) => {
+  const mailgun = new Mailgun(formData);
+
+  const mg = mailgun.client({
+    username: 'api',
+    key: process.env.MAILGUN_API_KEY
+  });
+
+  const { originalname, buffer } = req.file;
+
+  const emailToSend = {
+    from: process.env.ADDRESS_FROM,
+    to: process.env.ADDRESS_TO,
+    subject: 'DEMO EMAIL: Next.js + Uploads + Mailgun',
+    attachment: {
+      filename: originalname,
+      data: buffer
+    },
+    html: `<p style="white-space: pre-line">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos quis blanditiis aliquam ratione, explicabo non dolorem natus dignissimos magni ullam aut dolore ea qui voluptatem illo unde deserunt totam!</p>`
+  };
+
+  await mg.messages.create(process.env.MAILGUN_DOMAIN, emailToSend);
+
+  res.status(200).json({ status: 'success' });
+});
+
+export default apiRoute;
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const fileInputRef = React.useRef(null);
+  const formRef = React.useRef(null);
+
+  const onClickHandler = () => {
+    fileInputRef.current?.click();
+  };
+
+  const onChange = async (formData) => {
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' },
+      onUploadProgress: (event) => {
+        console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+      }
+    };
+
+    const response = await axios.post('/api/uploads', formData, config);
+
+    if (response.data.status === 'success') {
+      alert('Check your email!');
+    }
+
+    console.log('response', response.data);
+  };
+
+  const onChangeHandler = (event) => {
+    if (!event.target.files?.length) {
+      return;
+    }
+
+    const formData = new FormData();
+
+    Array.from(event.target.files).forEach((file) => {
+      formData.append(event.target.name, file);
+    });
+
+    onChange(formData);
+
+    formRef.current?.reset();
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div>
+        <form ref={formRef}>
+          <button
+            type="button"
+            className="inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={onClickHandler}>
+            Select file + Send email
+          </button>
+          <input
+            accept=""
+            multiple={false}
+            name="attachment"
+            onChange={onChangeHandler}
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            type="file"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
